f24cecf58f5d14d57ee3afd0a200a4e012b8b6b7
coderj001 <amirajubolchi001@gmail.com>
2022-01-18 22:51:10 +0530

    restart project

--- init.lua
+++ init.lua
@@ -1,12 +1,7 @@
 -- require
 
-require("plugin")
-require("syntax")
-require("mapping")
-require("functions")
-require("options")
-
-local gl_user = require("global").user
-if gl_user ~= nil then
-	print("Welcome, ", gl.user)
-end
+require("package")
+-- require("syntax")
+-- require("mapping")
+-- require("functions")
+-- require("options")
--- lua/config/colorscheme.lua
+++
@@ -1,7 +0,0 @@
-local M = {}
-
-function M.setup()
-    vim.cmd("colorscheme dracula")
-end
-
-return M
