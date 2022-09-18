e4b12b40ce7ca59c7e89cc175f202c8d97ad4a3e
raju ghorai <raju@admins-MacBook-Pro.local>
2022-09-18 23:11:12 +0530

    added mac config changes

--- init.lua
+++ init.lua
@@ -1,14 +1,9 @@
-if (not vim.g.vscode) then
-    -- require
-    require "user.plugin"
-    require "user.options"
-    require "user.functions"
-    require "user.keybindings"
-    require "user.cmp"
-    require "user.lsp"
-
-    -- Plugin
-    require "plugin.autopairs"
-    require "plugin.gitsigns"
-    require "plugin.rest"
-end
+require "user.plugin"
+require "user.options"
+require "user.functions"
+require "user.keybindings"
+require "user.cmp"
+require "user.lsp"
+require "plugin.autopairs"
+require "plugin.gitsigns"
+require "plugin.rest"
--- lua/plugin/colorschemes.lua
+++ lua/plugin/colorschemes.lua
@@ -2,20 +2,7 @@ local colorbuddy = require("colorbuddy")
 local M = {}
 
 function M.setup()
-    require("onedarkpro").setup({
-        dark_theme = "onedark",
-        light_theme = "onelight",
-        styles = {
-            comments = "italic",
-            functions = "bold",
-            keywords = "bold,italic",
-            strings = "NONE",
-            variables = "NONE",
-            virtual_text = "NONE"
-        },
-        options = {transparency = true, underline = true, undercurl = true}
-    })
-    colorbuddy.colorscheme('onedarkpro')
+    colorbuddy.colorscheme('nightfox')
     colorbuddy.setup()
 end
 
